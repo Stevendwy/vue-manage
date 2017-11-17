b6e81f770422930368fb78cd30816b38765d48f3
steven <steven@peipeiyun.com>
2017-11-17 14:53:06 +0800

    vue 后台系统
    框架搭建完成

--- src/main.js
+++ src/main.js
@@ -4,12 +4,18 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
-Vue.config.productionTip = false
+import axios from 'axios'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-default/index.css'    // 默认主题
+// import '../static/css/theme-green/index.css'       // 浅绿色主题
+import "babel-polyfill"
+
+// Vue.config.productionTip = false
+Vue.use(Element)
+Vue.prototype.$axios = axios
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
   router,
-  template: '<App/>',
-  components: { App }
-})
+  render:h=>h(App)
+}).$mount("#app")
